a08b20a3457c2f7a816938fe8776c4e516e901d7
dem123456789 <diao_em@hotmail.com>
2024-05-16 13:56:02 +0800

    update sep_data

--- dataset/data_preprocess/sep_data.py
+++ dataset/data_preprocess/sep_data.py
@@ -3,36 +3,23 @@ import os
 import pandas as pd
 
 
-def sep_data_by_d_name(directory, extension):
-    processed_data_path = os.path.join(directory, 'processed_data')
-    if not os.path.exists(processed_data_path):
-        os.mkdir(processed_data_path)
-    file_paths = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(extension):
-                file_paths.append(os.path.join(root, file))
-    env_path = next((str(i) for i in file_paths if 'env' in i), None)
-    file_paths.remove(env_path)
+def sep_data_by_d_name(path, time_range, extension='csv'):
+    time_range_data_path = os.path.join(path, time_range)
+    env_path = os.path.join(path, 'env.csv')
     env = pd.read_csv(env_path)
     env = env[env['level'] != 1]
     d_name_list = list(env['d_name'])
 
-    for data_path_i in file_paths:
-        data = pd.read_csv(str(data_path_i))
-        for d_name in d_name_list:
-            data_i = data[data['d_name'] == d_name]
-            if not data_i.empty:
-                save_path = os.path.join(processed_data_path, os.path.basename(data_path_i).replace('.csv', '') + '-' + d_name)
-                data_i.to_csv(save_path, index=False)
-
+    filenames = os.listdir(time_range_data_path)
+    for filename in filenames:
+        if filename.split('.')[-1] == extension:
+            data = pd.read_csv(os.path.join(time_range_data_path, filename))
+            for d_name in d_name_list:
+                data_i = data[data['d_name'] == d_name]
+                if not data_i.empty:
+                    save_path_i = os.path.join(time_range_data_path, 'device', '{}.csv'.format(d_name))
+                    data_i.to_csv(save_path_i, index=False)
+    return
 
 if __name__ == '__main__':
-    sep_data_by_d_name('hh105', 'csv')
-
-
-
-
-
-
-
+    sep_data_by_d_name('hh105', 'year')
